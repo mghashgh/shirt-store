12e4572350eee421c83d237b6eb949c935a3192c
Mehrnaz <mehrnaz.ghashghaei@gmail.com>
2025-02-06 11:50:38 -0800

    hook up backend to front end

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,25 +1,39 @@
-import React, { useState } from 'react';
-import './App.css';
-import { ShirtCard } from './components/ShirtCard.tsx';
-import { OrderSummary } from './components/OrderSummary.tsx';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { ShirtCard } from "./components/ShirtCard.tsx";
+import { OrderSummary } from "./components/OrderSummary.tsx";
 
 function App() {
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
-  const [count3, setCount3] = useState(0);
-  const [count4, setCount4] = useState(0);
-  const [count5, setCount5] = useState(0);
+  const [shirt1, setShirt1] = useState(0);
+  const [shirt2, setShirt2] = useState(0);
+  const [shirt3, setShirt3] = useState(0);
+  const [shirt4, setShirt4] = useState(0);
+  const [shirt5, setShirt5] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/total", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ shirt1, shirt2, shirt3, shirt4, shirt5 }),
+    }).then((response) => response.json())
+      .then((data) => setTotal(data.total))
+      .catch((error) => console.log(error));
+  }, [shirt1, shirt2, shirt3, shirt4, shirt5]);
+
   return (
     <div className="App">
       <h1>Welcome to Shirt Store</h1>
       <div className="cards-container">
-        <ShirtCard name={1} count={count1} setCount={setCount1}/>
-        <ShirtCard name={2} count={count2} setCount={setCount2}/>
-        <ShirtCard name={3} count={count3} setCount={setCount3}/>
-        <ShirtCard name={4} count={count4} setCount={setCount4}/>
-        <ShirtCard name={5} count={count5} setCount={setCount5}/>
+        <ShirtCard name={1} count={shirt1} setCount={setShirt1} />
+        <ShirtCard name={2} count={shirt2} setCount={setShirt2} />
+        <ShirtCard name={3} count={shirt3} setCount={setShirt3} />
+        <ShirtCard name={4} count={shirt4} setCount={setShirt4} />
+        <ShirtCard name={5} count={shirt5} setCount={setShirt5} />
       </div>
-      <OrderSummary total={53.40}/>
+      <OrderSummary total={total} />
     </div>
   );
 }
